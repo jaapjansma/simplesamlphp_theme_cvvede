2d9e708650527e12b4c57c5ca51c48e5420d4789
Jaap Jansma <jaap@edeveloper.nl>
2017-05-20 10:34:36 +0200

    initial commit

---
+++ themes/cvve/default/includes/footer.php
@@ -0,0 +1,13 @@
+<?php
+if(!empty($this->data['htmlinject']['htmlContentPost'])) {
+	foreach($this->data['htmlinject']['htmlContentPost'] AS $c) {
+		echo $c;
+	}
+}
+?>
+	</div><!-- #content -->
+
+</div><!-- #wrap -->
+
+</body>
+</html>
